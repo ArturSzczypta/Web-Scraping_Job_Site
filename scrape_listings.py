78749dab3637caf2e0916a949b9531b8fc188753
ArturSzczypta <arturszczypta@gmail.com>
2023-03-12 14:40:24 +0100

    Starting to test JSON when parsed

--- scrape_listings.py
+++ scrape_listings.py
@@ -11,10 +11,57 @@ import logging
 from logging import config
 import logging_functions as l
 
-def scrape_single_listing(url):
-    try:
-        ''' Scrapes job listing details from the url, return a dictionary'''
+def replace_empty(data_string):
+    """
+    Replace empty dictionaries, lists and strings with None
+    """
+    patterns = [r':\s*""', r':\s*\[\]', r':\s*{}', r':\s*,', r':\s*]', r':\s*\}', r':\s*\{\}']
+    for pattern in patterns:
+        data_string = re.sub(pattern, ':null', data_string)
+    print(data_string)
+    return data_string
 
+def clean_dict(my_dict):
+    '''
+    Removes all unnecesarry characters and symbols
+    Replace " " inside strings with ( )
+    These dictionaries are very complex, therefore recursion is used
+    '''
+    for key, val in my_dict.items():
+        if isinstance(val, str):
+            # Remove sequences
+            val = re.sub(r'\\n|\\t|\\r', '', val)
+            # Replace "some text" with (some text)
+            val = re.sub(r'"(.*?)"', r'(\1)', val)
+            # Replace any non-alphanumeric or non-allowed characters with space
+            val = re.sub(r'[^\w,:\.\'"\-(){}\[\]\sąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+', ' ', val)
+            # Remove leading or trailing whitespaces, change empty strings to None
+            my_dict[key] = val.strip() if val.strip() != '' else None
+        elif isinstance(val, list):
+            if not val:
+                my_dict[key] = None
+            else:
+                # Process each item in the list recursively
+                for i in range(len(val)):
+                    val[i] = clean_dict(val[i])
+                my_dict[key] = val
+                # If now empty, change to None
+                if not val:
+                    my_dict[key] = None
+        elif isinstance(val, dict):
+            try:
+                # Process the nested dictionary recursively
+                cleaned_val = clean_dict(val)
+                # If dictionery is empty, change to None
+                my_dict[key] = cleaned_val if any(cleaned_val.values()) else None
+            except AttributeError:
+                print(f"Error with key: {key}, value: {val}")
+                print('\n')
+    return my_dict
+
+def scrape_single_listing(url):
+    ''' Scrapes job listing details from the url, return a dictionary'''
+    try:
         # Make a request to the URL and get the HTML response
         response = requests.get(url)
         response.encoding = 'utf-8' # To recognise polish letters
@@ -26,23 +73,109 @@ def scrape_single_listing(url):
         start_index = response.text.find(start_string) + len(start_string)
         end_index = response.text.find(end_string, start_index)
         substring = response.text[start_index:end_index]
-
-        # Replace invalid valiable name with null
-        fixed_json_str = re.sub(r'\bundefined\b', 'null', substring)
+        # Replace invalid valiable names with null, so i can create dictionary
+        substring = re.sub(r'\bundefined\b', '', substring)
+        #print(substring +'\n')
+        # Replace empty dictionaries, lists and strings with None
+        substring = replace_empty(substring)
+        print(substring)
 
         # Convert the string to a dictionary using the json module
-        my_dict = json.loads(fixed_json_str)
-        new_dict = {'url': url}
-        my_dict = {**new_dict, **my_dict}
-
-        json_str = json.dumps(my_dict)
-        json_str = json_str.replace('\n', '').replace(' ', '')
-        return json.dumps(my_dict, ensure_ascii=False)
+        my_dict = json.loads(substring)
+        return my_dict
     except:
-        print(url)
+        print(f'failed to get JSON from url {url}')
+        print('\n\n')
 
+def extract_data(my_dict, url):
+    ''' Extracts usefull data from the dictionary, creates new dictionary'''
+    try:
+        #Basic listing data
+        job_title = my_dict['offerReducer']['offer']['jobTitle']
+        country = my_dict['offerReducer']['offer']['workplaces'][0]['country']['name']
+        region = my_dict['offerReducer']['offer']['workplaces'][0]['region']['name']
+        location = my_dict['offerReducer']['offer']['workplaces'][0]['inlandLocation']['location']['name']
+        salary = my_dict['offerReducer']['offer']['typesOfContracts'][0]['salary']
+
+        # Assuming both dates always comply to ISO 8601 format, UTC time zone
+        publication_date = datetime.datetime.fromisoformat(my_dict['offerReducer']['offer']['dateOfInitialPublication']).date()
+        expiration_date = datetime.datetime.fromisoformat(my_dict['offerReducer']['offer']['expirationDate']).date()
+
+        # technologies
+        tech_expected = {tech['name'] for tech in my_dict['offerReducer']['offer']['sections'][0]['subSections'][0]['model']['customItems']}
+        tech_optional = None
+        if len(my_dict['offerReducer']['offer']['sections'][0]['subSections']) > 1:
+            tech_optional = {tech['name'] for tech in my_dict['offerReducer']['offer']['sections'][0]['subSections'][1]['model']['customItems']}
+
+        # responsibilities
+        resp_expected = {resp for resp in my_dict['offerReducer']['offer']['sections'][1]['model']['bullets']}
+
+        # requirements
+        req_expected = {resp for resp in my_dict['offerReducer']['offer']['sections'][2]['subSections'][0]['model']['bullets']}
+        req_optional = {resp for resp in my_dict['offerReducer']['offer']['sections'][2]['subSections'][1]['model']['bullets']}
+
+        # development-practices
+        dev_practices = None
+        if 'items' in my_dict['offerReducer']['offer']['sections'][4]['model']:
+            dev_practices = {practices['code'] for practices in my_dict['offerReducer']['offer']['sections'][4]['model']['items']}
+
+        #Creating a new, simplified dictionary for saving
+        new_dict = {}
+        #Basic listing data
+        new_dict['url'] = url
+        new_dict['job_title'] = job_title
+        new_dict['country'] = country
+        new_dict['region'] = region
+        new_dict['location'] = location
+        new_dict['salary'] = salary
+        # Assuming both dates always comply to ISO 8601 format, UTC time zone
+        new_dict['publication_date'] = publication_date
+        new_dict['expiration_date'] = expiration_date
+        # technologies
+        new_dict['tech_expected'] = tech_expected
+        new_dict['tech_optional'] = tech_optional
+        # responsibilities
+        new_dict['resp_expected'] = resp_expected
+        # requirements
+        new_dict['req_expected'] = req_expected
+        new_dict['req_optional'] = req_optional
+        # development-practices
+        new_dict['dev_practices'] = dev_practices
+        #print(new_dict)
+        return new_dict
+    except:
+        print(f'Extraction failed ') #{my_dict}
+
+def save_dict(new_dict,file_name):
+    ''' Saves dictionary to file'''
+    json_str = json.dumps(new_dict, ensure_ascii=False)
+
+    with open(file_name, 'a', encoding='utf-8') as file:
+        file.write(json_str + '\n')
+
+def main(url, file_name):
+    '''Runs if script called directly'''
+    my_dict = scrape_single_listing(url)
+    #print(my_dict)
+    my_dict = clean_dict(my_dict)
+    print(my_dict)
+    new_dict = extract_data(my_dict, url)
+    print(new_dict)
+    save_dict(new_dict,file_name)
+
+if __name__ == '__main__':
+    url = 'https://www.pracuj.pl/praca/poznaj-nas-na-festiwalu-pracy-jobicon-krakow,oferta,1002447086'
+    file_name = 'extracted_dict_examples.txt'
+    main(url, file_name)
+
+
+
+
+
+
+'''
 def scrape_all_listings_data():
-    ''' Scrapes json data about job listings, save to text file'''
+    Scrapes json data about job listings, save to text file
     unique_links = set()
     finished_links = set()
     finished_count = 0
@@ -77,7 +210,7 @@ def scrape_all_listings_data():
             print(finished_count / one_percent)
 
 def extract_usefull_listings(skills_list):
-    ''' Extract listings that have my skills '''
+    Extract listings that have my skills
     # Patterns for skills_slist = ['Python', 'SQL', 'R']
     # Designed to find SQL
     pattern_1 = r'%s'
@@ -106,33 +239,4 @@ def extract_usefull_listings(skills_list):
         # Write the matching lines to the output file
         for match in matches:
             output_file.write(match)
-
-def main():
-        ''' Performs basic logging set up, if script is runned directly'''
-        '''
-        #Get this script name
-        log_file_name = __file__.split('\\')
-        log_file_name = f'{log_file_name[-1][:-3]}_log.log'
-
-        l.get_log_file_name(log_file_name)
-
-        #Configure logging file 
-        l.configure_logging()
-        logger = logging.getLogger('main')
-
-        #Check internet connection, terminate script if no internet
-        l.check_internet()
-        '''
-
-        print('running')
-        skills_list = ['Python', 'SQL', 'R']
-        extract_usefull_listings(skills_list)
-        '''
-        When first Tested:
-        Pattern Python: 1830 unique matches
-        Pattern SQL: 3754 unique matches
-        Pattern \bR\b: 710 unique matches
-        '''
-
-if __name__ == '__main__':
-        main()
+'''
