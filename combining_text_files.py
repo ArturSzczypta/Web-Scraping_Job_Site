9bcdb127213081b3e89cb98f75caca21c1a9b4b3
ArturSzczypta <arturszczypta@gmail.com>
2023-05-04 22:10:03 +0200

    Cleaning unfinished scrapings part 1

--- combining_text_files.py
+++ combining_text_files.py
@@ -17,3 +17,15 @@ def urls_to_set(file_name, urls_set):
     with open(file_name, 'r', encoding='utf-8') as file:
         for line in file:
             urls_set.add(line.strip())
+
+succesfull_to_set('succesfull_extractions_0.txt')
+succesfull_to_set('succesfull_extractions_1.txt')
+succesfull_to_set('succesfull_extractions_2.txt')
+
+urls_to_set('failed_extractions_0.txt', failed_urls_set)
+urls_to_set('failed_extractions_1.txt', failed_urls_set)
+
+urls_to_set('mixed_extractions_0.txt', mixed_urls_set)
+urls_to_set('mixed_extractions_1.txt', mixed_urls_set)
+
+failed_urls_set.update(mixed_urls_set.difference(succesfull_urls_set))
